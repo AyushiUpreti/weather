7c02d0c2e2ea07b1e9b697b59f69f0cbf19185cc
AyushiUpreti <ayushiupreti7@gmail.com>
2023-03-25 15:16:07 +0530

    weather

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {usestate} from "react";
+import axios from "axios";
 
 function App() {
+
+  // const url= `https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=39333f62ed1497e5a4ff008f28e99cb3`
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="app">
+    <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>Delhi</p>
+        </div>
+        <div className="temperature">
+          <h1>25°C</h1>
+        </div>
+        <div className="description">
+          <p>Cloudy</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p className="bold">25°C</p>
+          <p>Feels like</p>
+        </div>
+        <div className="humidity">
+          <p className="bold">20%</p>
+          <p>Humidity</p>
+        </div>
+        <div className="wind">
+          <p className="bold">12 MPH</p>
+          <p>Winds</p>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
 
